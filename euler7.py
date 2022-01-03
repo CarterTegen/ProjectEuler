6939f0172eef1a7fc63e9c2cfe1dae9ad68aa068
Carter Tegen <carter@tegen.net>
2022-01-02 21:14:50 -0500

    Prob 1-11

---
+++ euler7.py
@@ -0,0 +1,29 @@
+def is_prime(num):
+    if num > 1:
+  
+        # Iterate from 2 to n / 2
+        for i in range(2, int(num/2)+1):
+    
+            # If num is divisible by any number between
+            # 2 and n / 2, it is not prime
+            if (num % i) == 0:
+                return False
+                break
+        else:
+            return True
+    
+    else:
+        return False
+
+
+if __name__ == '__main__':
+
+    numPrime = 1
+    n = 2
+
+    while numPrime <= 5:
+        if is_prime(n):
+            print(numPrime, n)
+            numPrime += 1
+        
+        n += 2
