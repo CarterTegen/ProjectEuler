593bcbc92b2a0f8b21f58f80d9bb06a71a6c3865
Carter Tegen <carter@tegen.net>
2022-01-03 20:03:01 -0500

    Added euler15

---
+++ solutions/11-19/euler15.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n==1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+n = 20
+lattice_paths = int(factorial(2*n)/(factorial(n)**2))
+print(lattice_paths)
